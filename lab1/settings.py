b1db4240ec384ff9a44652d210bae67804c6a78c
Josh Hall <jhall38@uw.edu>
2016-07-31 21:10:25 +0000

    adds relational database

--- lab1/settings.py
+++ lab1/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'lab1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lab1',
+	'USER': 'lab1_admin',
+	'PASSWORD': 'admin',
+	'HOST': 'localhost',
+	'POST': '',
     }
 }
 
